b3e5bed471384c784901ff84e5f01ba03a87fc67
riziqnouval <108220333+riziqnouval@users.noreply.github.com>
2022-12-04 21:58:35 +0700

    Add files via upload

---
+++ TPMODUL2_RIZIQ/Laptop.java
@@ -0,0 +1,31 @@
+public class Laptop extends Perangkat {
+    protected boolean webcam;
+
+    public Laptop(String drive, int ram, float processor, boolean webcam){
+        super(drive, ram, processor);
+        this.webcam = webcam;
+    }
+
+    public void informasi(){
+        if (webcam){
+            System.out.println("Laptop ini memiliki drive tipe "+ drive +" dengan ram sebesar "+ ram +" GB dan processor secepat "+ processor +" Ghz. Selain itu laptop ini juga memiliki webcam");
+        }
+        else{
+            System.out.println("Laptop ini memiliki drive tipe "+ drive +" dengan ram sebesar "+ ram +" GB dan processor secepat "+ processor +" Ghz. Selain itu laptop ini juga TIDAK memiliki webcam");
+        }
+        
+    }
+
+    public void bukaGame(String nama_game){
+        System.out.println("Laptop berhasil membuka game "+ nama_game);
+    }
+
+    public void kirimEmail(String email){
+        System.out.println("Laptop berhasil mengirim Email ke "+ email);
+    }
+
+    public void kirimEmail(String email1, String email2){
+        System.out.println("Laptop berhasil mengirim Email ke "+ email1 +" dan ke "+ email2);
+    }
+    
+}
