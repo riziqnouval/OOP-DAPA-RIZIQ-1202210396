42f33a7813ff366c80a38dc0a09e189863394332
riziqnouval <108220333+riziqnouval@users.noreply.github.com>
2022-12-09 17:10:33 +0700

    Add files via upload

---
+++ MODUL2_RIZIQ/TransportasiAir.java
@@ -0,0 +1,22 @@
+public class TransportasiAir {
+    protected int jumlahKursi;
+    protected int biaya;
+
+    public TransportasiAir(int jumlahKursi, int biaya) {
+        this.jumlahKursi = jumlahKursi;
+        this.biaya = biaya;
+    }
+
+    public void informasi() {
+        System.out.println();
+    }
+
+    public void berlayar() {
+        System.out.println();
+    }
+
+    public void berlabuh() {
+        System.out.println();
+    }
+
+}
