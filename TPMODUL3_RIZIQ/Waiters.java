495bd9ea556e5a0f48e9077c6d06612bb739d44e
riziqnouval <108220333+riziqnouval@users.noreply.github.com>
2022-12-25 20:11:29 +0700

    Add files via upload

---
+++ TPMODUL3_RIZIQ/Waiters.java
@@ -0,0 +1,42 @@
+public class Waiters implements Runnable{
+    private final int orderQty;
+    private final int customerID;
+    static int foodPrice = 25000;
+
+    public Waiters(int customerID, int orderQty){
+        this.customerID = customerID;
+        this.orderQty = orderQty;
+    }
+    @Override
+    public void run(){
+        while(true){
+            makeFood();
+            try{
+                Thread.sleep(15000);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+    public void orderInfo(){
+        System.out.println("=======================================");
+        System.out.println("Customer ID: " + this.customerID);
+        System.out.println("Number of Food: " + this.orderQty);
+        System.out.println("Total Price: " + this.orderQty*foodPrice);
+        System.out.println("=======================================");
+    }
+    public void makeFood(){
+        synchronized(Restaurant.getLock()){
+            System.out.println("Waiter: Food is ready to deliver");
+            Restaurant restaurant = new Restaurant();
+            restaurant.setWaitingForPickup(false);
+
+            if (Restaurant.getFoodNumber() == this.orderQty + 1){
+                orderInfo();
+                System.exit(0);
+            }
+            Restaurant.getLock().notifyAll();
+            System.out.println("Waiter: Tell the restaurant to make another food\n");
+        }
+    }
+}
